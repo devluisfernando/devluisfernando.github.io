8eb6e47bd5ee2c4a8e600cfbaf4575eaf42c9fcc
devluisfernando <160809680+devluisfernando@users.noreply.github.com>
2024-02-22 16:33:41 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
-// toggle icon navbar
+document.addEventListener("DOMContentLoaded", function () {
+  let homeSection = document.querySelector('.home');
+  homeSection.classList.add('show-animate');
+});
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
-
-// scroll sections
-let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // sticky header
-
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-
-
-    // animation footer on scroll
-
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
+
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+      sec.classList.add('show-animate');
+    } else {
+      sec.classList.remove('show-animate');
+    }
+  });
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+  let footer = document.querySelector('footer');
+
+  footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
+
+// Adicione esta parte do código para fechar o menu ao clicar em um link da navbar
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  });
+});
